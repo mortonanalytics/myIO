1dcf3d4cbc18b4cca934e047efe8259d0df1868f
Ryan Morton <mortonanalytics@gmail.com>
2018-06-03 17:08:02 -0600

    add tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(myIO)
+
+test_check("myIO")
---
+++ tests/testthat/test-layer.R
@@ -0,0 +1,26 @@
+library(testthat)
+
+context("add layer results")
+testObject <-  myIO() %>%
+  addIoLayer(type = 'point',
+           color = "blue",
+           label = "testLayer",
+           data = mtcars,
+           mapping = list(x_var = "wt",
+                               y_var = "mpg"))
+
+test_that("add layer creates a list of two", {
+  expect_output(str(testObject$x$layers), "List of 1")
+})
+
+testObject2 <- testObject %>%
+  addIoLayer(type = "point",
+           color = "red",
+           data = mtcars,
+           label = "newLayer",
+           mapping = list(x_var = "drat",
+                               y_var = "wt"))
+
+test_that("add layer creates a list of two", {
+  expect_output(str(testObject2$x$layers), "List of 2")
+})
