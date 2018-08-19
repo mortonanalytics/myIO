bf8b7c7735c3f85a4688c3187b2a229094f5149f
Ryan Morton <mortonanalytics@gmail.com>
2018-08-19 16:04:51 -0600

    add function testing
    output for IO layer, IO stat layer, and dragPoints

---
+++ tests/testthat/test_layer.R
@@ -0,0 +1,31 @@
+# test IO layer
+test_object <- myIO::addIoLayer(myIO::myIO(),
+                 type = "line",
+                 label = "test_line",
+                 color = "red",
+                 data = mtcars,
+                 mapping = list(x_var = "wt",
+                                y_var = "mpg"))
+
+testthat::test_that("add layer creates a list of two", {
+  expect_output(str(test_object$x$layers), "List of 1")
+})
+
+#test drag points with previous layer
+test_object <- myIO::dragPoints(test_object)
+
+testthat::test_that("dragPoints adds option to list", {
+  expect_output(str(test_object$x$options$dragPoints), "TRUE")
+})
+
+#test IO Stat layer
+test_stat_object <- myIO::addIoStatLayer(myIO::myIO(),
+                                         type = "lm",
+                                         label = "linear_test",
+                                         color = "red",
+                                         data = mtcars,
+                                         mapping = list(x_var = "wt",
+                                                        y_var = "mpg"))
+testthat::test_that("add layer creates a list of two", {
+  expect_output(str(test_stat_object$x$layers), "List of 1")
+})
