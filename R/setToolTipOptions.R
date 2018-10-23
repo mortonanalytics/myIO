f7747e8cf204b78961238bcadde4cc56a1f3858c
Ryan Morton <mortonanalytics@gmail.com>
2018-10-23 11:27:43 -0600

    add tool tip options function to suppress Y values

---
+++ R/setToolTipOptions.R
@@ -0,0 +1,18 @@
+#' setToolTipOptions()
+#'
+#' Generic function for setting tool tip options for a chart
+#'
+#' @param myIO an htmlwidget object created by the myIO() function
+#' @param suppressY a boolean
+
+#'
+#' @return the same myIO object with options set for the tooltip formats
+#'
+#' @export
+setToolTipOptions <- function(myIO, suppressY = NULL){
+
+  myIO$x$options$toolTipOptions$suppressY <- suppressY
+
+
+  return(myIO)
+}
