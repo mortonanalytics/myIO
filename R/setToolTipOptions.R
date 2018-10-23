67987b8e3cb6ca2dd72dbcd282eb38c29433aded
Ryan Morton <mortonanalytics@gmail.com>
2018-10-23 11:29:12 -0600

    update set tool tip options

--- R/setToolTipOptions.R
+++ R/setToolTipOptions.R
@@ -4,7 +4,6 @@
 #'
 #' @param myIO an htmlwidget object created by the myIO() function
 #' @param suppressY a boolean
-
 #'
 #' @return the same myIO object with options set for the tooltip formats
 #'
