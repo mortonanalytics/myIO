2996f3cacb62cb970f3589d7d89b6a35d5c7f0e4
Ryan Morton <mortonanalytics@gmail.com>
2019-01-27 14:51:15 -0700

    debug treemap 3

--- inst/htmlwidgets/myIO/myIOapi.js
+++ inst/htmlwidgets/myIO/myIOapi.js
@@ -441,7 +441,7 @@ chart.prototype.routeLayers = function() {
 		if(layerType == "line") {
 			if(d.mapping.low_y) {that.addArea(d);}
 			//that.addLine(d);
-			that.addPoints(d);
+			//that.addPoints(d);
 		} else if(layerType == "point") {
 			if(d.mapping.low_y) { that.addCrosshairsY(d); }
 			if(d.mapping.low_x) { that.addCrosshairsX(d); }
