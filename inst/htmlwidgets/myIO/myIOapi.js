afe42511e10c498cee81b246382d5dfa83b215d9
Ryan Morton <mortonanalytics@gmail.com>
2020-03-12 11:02:16 -0600

    browser compatitility bug fixes

--- inst/htmlwidgets/myIO/myIOapi.js
+++ inst/htmlwidgets/myIO/myIOapi.js
@@ -2344,6 +2344,27 @@ function svgString2Image( svgString, width, height, format, callback ) {
 	image.src = imgsrc;
 }
 
+if (!HTMLCanvasElement.prototype.toBlob) {
+  Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+    value: function (callback, type, quality) {
+      var dataURL = this.toDataURL(type, quality).split(',')[1];
+      setTimeout(function() {
+
+        var binStr = atob( dataURL ),
+            len = binStr.length,
+            arr = new Uint8Array(len);
+
+        for (var i = 0; i < len; i++ ) {
+          arr[i] = binStr.charCodeAt(i);
+        }
+
+        callback( new Blob( [arr], {type: type || 'image/png'} ) );
+
+      });
+    }
+  });
+}
+
 function exportToCsv(filename, rows) {
 	
 	var jsonObject = JSON.stringify(rows);
